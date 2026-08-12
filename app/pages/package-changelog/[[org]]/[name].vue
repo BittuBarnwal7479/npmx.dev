27f5bd769617dc7b23e986ba236b9cf04874058d
Wilco <17604138+WilcoSp@users.noreply.github.com>
2026-08-12 10:24:52 +0000

    feat: changelog - formatted links, more git providers & copy markdown (#2983)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Willow (GHOST) <git@willow.sh>

--- app/pages/package-changelog/[[org]]/[name].vue
+++ app/pages/package-changelog/[[org]]/[name].vue
@@ -130,8 +130,8 @@ defineOgImage(
         >
           {{ changelog.provider }}
         </LinkBase>
-        <div v-if="changelog?.type === 'md'" ref="tptoc" class="w-14 h-8">
-          <!-- prevents layout shift while loading -->
+        <div v-if="changelog?.type === 'md'" ref="tptoc" class="min-w-[150px] h-8 flex gap-2">
+          <!-- Placeholder for toc & copy md -->
         </div>
       </div>
       <section v-if="!changelog && !changelogError" class="flex flex-col gap-2 py-3">
